cf836d3c8816a17eb4adbce3bfa90b5efbb00748
Prokhor Portugalsky <prohor.luv@gmail.com>
2023-04-01 15:59:34 +0100

    2.3 Боковое меню

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <v-app>
-        <v-navigation-drawer app></v-navigation-drawer>
+        <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
         <v-app-bar app dark color="primary">
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
                 <v-btn>
@@ -20,6 +20,10 @@
 
 <script>
 export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  }
 }
-
 </script>
