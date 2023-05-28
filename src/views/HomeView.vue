163779819897b7d8dee3c098e870b79c0e331045
Prokhor Portugalsky <prohor.luv@gmail.com>
2023-05-28 16:28:48 +0100

    5.5 Данные к сетке

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,43 @@
 <template>
-	<v-container>
-		<v-row justify="center">
-			<v-col cols="12" xs="12">
-				<v-carousel>
-					<v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src" cover>
-						<div class="ad-link">
-							<v-btn class="error" :to="'/ad/' + ad.id">
-								{{ ad.title }}
+		<v-container>
+			<v-row justify="center">
+				<v-col cols="12" xs="12">
+					<v-carousel>
+						<v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src" cover>
+							<div class="ad-link">
+								<v-btn class="error" :to="'/ad/' + ad.id">
+									{{ ad.title }}
+								</v-btn>
+							</div>
+						</v-carousel-item>
+					</v-carousel>
+				</v-col>
+			</v-row>
+		</v-container>
+		<v-container grid-list-lg>
+			<v-row wrap> 
+				<v-col cols="12" xs="12" sm="6" md="4" v-for="ad in ads" :key="ad.id">
+					<v-card>
+						<v-img :src="ad.src" height="200px"></v-img>
+						<v-card-title primary-title>
+							<div>
+								<h3 class="headline mb-0">{{ad.title}}</h3>
+								<div>{{ad.desc}}</div>
+							</div>
+						</v-card-title>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn :to="'/ad/' + ad.id">
+								Open
 							</v-btn>
-						</div>
-						<component :is="'style'">
-							.ad-link {
-								position:absolute;
-								bottom: 50px;
-								left:50%;
-								background: rgb(0,0,0,0.5);
-								transform: translate(-50%,0);
-								padding: 5px 15px;
-								border-top-right-radius: 5px;
-								border-top-left-radius: 5px;
-							}
-						</component>
-
-					</v-carousel-item>
-				</v-carousel>
-			</v-col>
-		</v-row>
-	</v-container>
-	<v-container>
-		<v-row justify="center">
-			<v-col cols="12" xs="12">
-				<v-card>
-					<v-img height="200px"></v-img>
-					<v-card-title primary-title>
-						<div>
-							<h3 class="headline mb0">Здесь будет заголовок</h3>
-							<div>Здесь будет описание</div>
-						</div>
-					</v-card-title>
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn text>Open</v-btn>
-						<v-btn raised color="primary">
-							Buy
-						</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-col>
-		</v-row>
-	</v-container>
+							<v-btn raised color="primary">
+								Buy
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
 </template>
 	
 
@@ -91,3 +80,15 @@
 	} 
 
 </script>
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 15px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
