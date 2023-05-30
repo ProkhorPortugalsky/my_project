a62c6c3a23525c9ee67f90dbe64f91e76b86163b
Prokhor Portugalsky <prohor.luv@gmail.com>
2023-05-30 16:52:12 +0100

    8.2 Подключение данных

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -9,11 +9,11 @@
 							<v-img :src="ad.src" height="175px" cover></v-img>
 						</v-col>
 						<v-col xs="8">
-							<h2 class="text--primary">{{  }}</h2>
-							<p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{  }}</p>
+							<h2 class="text--primary">{{ad.title}}</h2>
+							<p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ad.desc}}</p>
 							<v-card-actions>
 								<v-spacer></v-spacer>
-								<v-btn color="primary" variant="text" :to="">
+								<v-btn color="primary" variant="text" :to="'/ad/' + ad.id">
 									Open
 								</v-btn>
 							</v-card-actions>
@@ -27,9 +27,40 @@
 
 <script>
 	export default {
-		data () {
+		data () { 
 			return {
-		} 
-	} 
+				ads:[
+					{
+						title:"First",
+						desc:"First Desc",
+						promo: true,
+						src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+						id:"1"
+					},
+					{
+						title:"Second",
+						desc:"Second Desc",
+						promo: true,
+						src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+						id:"2"
+					},
+					{
+						title:"Third",
+						desc:"Thitd Desc",
+						promo: true,
+						src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+						id:"3"
+					},
+					{
+						title:"Fouth",
+						desc:"Fouth Desc",
+						promo: true,
+						src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+						id:"4"
+					}
+			]
+		}
+	}
 }
+
 </script>
