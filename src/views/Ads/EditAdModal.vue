5ab61685c0a5b72108048091333c7e17f54e0907
Prokhor Portugalsky <prohor.luv@gmail.com>
2023-06-16 16:10:52 +0100

    17.3 Верстка полей

--- src/views/Ads/EditAdModal.vue
+++ src/views/Ads/EditAdModal.vue
@@ -15,7 +15,8 @@
                 <v-row justify="center">
                     <v-col cols="12">
                         <v-card-text>
-                            Lorem ipsum.
+                            <v-text-field name="title" label="Title" type="text" v-model="editedTitle"></v-text-field>
+                            <v-textarea  name="desc" label="Description" type="text" v-model="editedDesc" class="mb-3"></v-textarea>
                         </v-card-text>
                     </v-col>
                 </v-row>
@@ -36,7 +37,9 @@
     export default {
         data() {
             return {
-                modal: false
+                modal: false,
+                editedTitle: "",
+                editedDesc: ""
             }
         }
     }
